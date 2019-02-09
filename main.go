c0799ead03aa9f086a529172081cd06ef8e05de2
Szabó, Péter <szaboopeeter@live.com>
2019-02-09 11:09:58 +0700

    Basic repl (#3)

--- main.go
+++ main.go
@@ -2,8 +2,19 @@ package main
 
 import (
 	"fmt"
+	"monkey-lang/repl"
+	"os"
+	"os/user"
 )
 
 func main() {
-	fmt.Printf("Hello world")
+	user, err := user.Current()
+
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("Hello %s! This is Monkey-lang!\n", user.Username)
+	fmt.Printf("Feel free to type commands.\n")
+	repl.Start(os.Stdin, os.Stdout)
 }
