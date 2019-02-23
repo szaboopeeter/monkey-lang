03592975572031223afb55f0f6450a68d7abd8ea
Péter Szabó <szaboopeeter@live.com>
2019-02-23 21:49:47 +0700

    Start parser

---
+++ parser/parser.go
@@ -0,0 +1,33 @@
+package parser
+
+import (
+	"monkey-lang/ast"
+	"monkey-lang/lexer"
+	"monkey-lang/token"
+)
+
+type Parser struct {
+	l *lexer.Lexer
+
+	currentToken token.Token
+	peekToken    token.Token
+}
+
+func New(l *lexer.Lexer) *Parser {
+	p := &Parser{l: l}
+
+	// Set currentToken and peekToken by reading twice
+	p.nextToken()
+	p.nextToken()
+
+	return p
+}
+
+func (p *Parser) nextToken() {
+	p.currentToken = p.peekToken
+	p.peekToken = p.l.NextToken()
+}
+
+func (p *Parser) ParseProgram() *ast.Program {
+	return nil
+}
