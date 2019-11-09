f05582f3c784ab5138917f5ec090941391e473cf
Péter Szabó <szaboopeeter@live.com>
2019-11-09 12:09:27 +0700

    Evaluate the '!' operator

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -18,10 +18,19 @@ func Eval(node ast.Node) object.Object {
 		return &object.Integer{Value: node.Value}
 	case *ast.Boolean:
 		return nativeBoolToBooleanObject(node.Value)
+	case *ast.PrefixExpression:
+		right := Eval(node.Right)
+		return evalPrefixExpression(node.Operator, right)
 	}
 
 	return nil
 }
+
+var (
+	NULL  = &object.Null{}
+	TRUE  = &object.Boolean{Value: true}
+	FALSE = &object.Boolean{Value: false}
+)
 
 func evalStatements(statements []ast.Statement) object.Object {
 	var result object.Object
@@ -33,15 +42,31 @@ func evalStatements(statements []ast.Statement) object.Object {
 	return result
 }
 
-var (
-	Null  = &object.Null{}
-	TRUE  = &object.Boolean{Value: true}
-	FALSE = &object.Boolean{Value: false}
-)
-
 func nativeBoolToBooleanObject(input bool) *object.Boolean {
 	if input {
 		return TRUE
 	}
 	return FALSE
+}
+
+func evalPrefixExpression(operator string, operand object.Object) object.Object {
+	switch operator {
+	case "!":
+		return evalBangOperatorExpression(operand)
+	default:
+		return nil
+	}
+}
+
+func evalBangOperatorExpression(operand object.Object) object.Object {
+	switch operand {
+	case TRUE:
+		return FALSE
+	case FALSE:
+		return TRUE
+	case NULL:
+		return TRUE
+	default:
+		return FALSE
+	}
 }
