9f6f29e45de00cc0c3821f8faca1ba49be8fc809
Péter Szabó <szaboopeeter@live.com>
2019-11-09 14:42:06 +0700

    Fix block statement evaluation to support returns

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -9,11 +9,11 @@ func Eval(node ast.Node) object.Object {
 	switch node := node.(type) {
 	// Statements
 	case *ast.Program:
-		return evalStatements(node.Statements)
+		return evalProgram(node.Statements)
 	case *ast.ExpressionStatement:
 		return Eval(node.Expression)
 	case *ast.BlockStatement:
-		return evalStatements(node.Statements)
+		return evalBlockStatement(node)
 	case *ast.ReturnStatement:
 		value := Eval(node.ReturnValue)
 		return &object.ReturnValue{Value: value}
@@ -43,7 +43,7 @@ var (
 	FALSE = &object.Boolean{Value: false}
 )
 
-func evalStatements(statements []ast.Statement) object.Object {
+func evalProgram(statements []ast.Statement) object.Object {
 	var result object.Object
 
 	for _, statement := range statements {
@@ -146,6 +146,19 @@ func evalIfExpression(ifExpression *ast.IfExpression) object.Object {
 	} else {
 		return NULL
 	}
+}
+
+func evalBlockStatement(block *ast.BlockStatement) object.Object {
+	var result object.Object
+
+	for _, statement := range block.Statements {
+		result = Eval(statement)
+
+		if result != nil && result.Type() == object.RETURN_VALUE_OBJ {
+			return result
+		}
+	}
+	return result
 }
 
 func isTruthy(obj object.Object) bool {
