585475c229c70183024923412920508a23f0493b
Péter Szabó <szaboopeeter@live.com>
2019-11-09 11:36:18 +0700

    Test integer literals

---
+++ evaluator/evaluator_test.go
@@ -0,0 +1,46 @@
+package evaluator
+
+import (
+	"monkey-lang/lexer"
+	"monkey-lang/object"
+	"monkey-lang/parser"
+	"testing"
+)
+
+func TestEvalIntegerExpression(t *testing.T) {
+	tests := []struct {
+		input    string
+		expected int64
+	}{
+		{"5", 5},
+		{"10", 10},
+	}
+
+	for _, tt := range tests {
+		evaluated := testEval(tt.input)
+		testIntegerObject(t, evaluated, tt.expected)
+	}
+}
+
+func testEval(input string) object.Object {
+	lexer := lexer.New(input)
+	parser := parser.New(lexer)
+	program := parser.ParseProgram()
+
+	return Eval(program)
+}
+
+func testIntegerObject(t *testing.T, obj object.Object, expected int64) bool {
+	result, ok := obj.(*object.Integer)
+	if !ok {
+		t.Errorf("object is not an Integer. got=%T (%+v)", obj, obj)
+		return false
+	}
+
+	if result.Value != expected {
+		t.Errorf("object has wrong value. Expected=%d, got=%d.", expected, result.Value)
+		return false
+	}
+
+	return true
+}
