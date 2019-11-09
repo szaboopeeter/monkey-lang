3521df29c70395351566f0e02e4ba0d211246650
Szabó, Péter <me@peterszabo.dev>
2019-11-09 11:55:42 +0700

    Create the Evaluator (#14)
    * integer, boolean and null type objects
    
    * Test integer literals
    
    * Evaluate integer literals (and program, and expressionstatements)
    
    * Adding the 'E' to the REPL
    
    * Test boolean literals
    
    * Evaluate boolean literals
    
    * "Intern" booleans
    
    * Create a single null reference

---
+++ evaluator/evaluator.go
@@ -0,0 +1,47 @@
+package evaluator
+
+import (
+	"monkey-lang/ast"
+	"monkey-lang/object"
+)
+
+func Eval(node ast.Node) object.Object {
+	switch node := node.(type) {
+	// Statements
+	case *ast.Program:
+		return evalStatements(node.Statements)
+	case *ast.ExpressionStatement:
+		return Eval(node.Expression)
+
+	// Expressions
+	case *ast.IntegerLiteral:
+		return &object.Integer{Value: node.Value}
+	case *ast.Boolean:
+		return nativeBoolToBooleanObject(node.Value)
+	}
+
+	return nil
+}
+
+func evalStatements(statements []ast.Statement) object.Object {
+	var result object.Object
+
+	for _, statement := range statements {
+		result = Eval(statement)
+	}
+
+	return result
+}
+
+var (
+	Null  = &object.Null{}
+	TRUE  = &object.Boolean{Value: true}
+	FALSE = &object.Boolean{Value: false}
+)
+
+func nativeBoolToBooleanObject(input bool) *object.Boolean {
+	if input {
+		return TRUE
+	}
+	return FALSE
+}
