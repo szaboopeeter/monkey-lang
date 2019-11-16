792a80e1783c5f25bef1af5f55df5dffa60e0e2b
Péter Szabó <szaboopeeter@live.com>
2019-11-16 13:00:11 +0700

    Evaluate array index expressions

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -76,6 +76,16 @@ func Eval(node ast.Node, environment *object.Environment) object.Object {
 		}
 
 		return applyFunction(function, args)
+	case *ast.IndexExpression:
+		left := Eval(node.Left, environment)
+		if isError(left) {
+			return left
+		}
+		index := Eval(node.Index, environment)
+		if isError(index) {
+			return index
+		}
+		return evalIndexExpression(left, index)
 	}
 
 	return nil
@@ -255,6 +265,28 @@ func evalExpressions(expressions []ast.Expression, environment *object.Environme
 	}
 
 	return result
+}
+
+func evalIndexExpression(left, index object.Object) object.Object {
+	switch {
+	case left.Type() == object.ARRAY_OBJ && index.Type() == object.INTEGER_OBJ:
+		return evalArrayIndexExpression(left, index)
+	default:
+		return newError("Index operator is not defined on type: %s", left.Type())
+	}
+}
+
+func evalArrayIndexExpression(array, index object.Object) object.Object {
+	arrayObject := array.(*object.Array)
+
+	idx := index.(*object.Integer).Value
+	max := int64(len(arrayObject.Elements) - 1)
+
+	if idx < 0 || idx > max {
+		return NULL
+	}
+
+	return arrayObject.Elements[idx]
 }
 
 func isTruthy(obj object.Object) bool {
