84a8d2d004fcb367daf8788fa1eab86b5189abc0
Péter Szabó <szaboopeeter@live.com>
2019-11-17 11:31:42 +0700

    Compiler infrastruction and simple behavior assertion

---
+++ compiler/compiler_test.go
@@ -0,0 +1,128 @@
+package compiler
+
+import (
+	"fmt"
+	"monkey-lang/ast"
+	"monkey-lang/code"
+	"monkey-lang/lexer"
+	"monkey-lang/object"
+	"monkey-lang/parser"
+	"testing"
+)
+
+func TestIntegerArithmetic(t *testing.T) {
+	tests := []compilerTestCase{
+		{
+			input:             "1 + 2",
+			expectedConstants: []interface{}{1, 2},
+			expectedInstructions: []code.Instructions{
+				code.Make(code.OpConstant, 0),
+				code.Make(code.OpConstant, 1),
+			},
+		},
+	}
+
+	runCompilerTests(t, tests)
+}
+
+type compilerTestCase struct {
+	input                string
+	expectedConstants    []interface{}
+	expectedInstructions []code.Instructions
+}
+
+func runCompilerTests(t *testing.T, tests []compilerTestCase) {
+	t.Helper()
+
+	for _, tt := range tests {
+		program := parse(tt.input)
+
+		compiler := New()
+		err := compiler.Compile(program)
+		if err != nil {
+			t.Fatalf("compiler error: %s", err)
+		}
+
+		bytecode := compiler.Bytecode()
+
+		err = testInstructions(tt.expectedInstructions, bytecode.Instructions)
+		if err != nil {
+			t.Fatalf("testInstructions failed: %s", err)
+		}
+
+		err = testConstants(t, tt.expectedConstants, bytecode.Constants)
+		if err != nil {
+			t.Fatalf("testConstants failed: %s", err)
+		}
+	}
+}
+
+func parse(input string) *ast.Program {
+	lexer := lexer.New(input)
+	parser := parser.New(lexer)
+	return parser.ParseProgram()
+}
+
+func testConstants(
+	t *testing.T,
+	expected []interface{},
+	actual []object.Object,
+) error {
+	if len(expected) != len(actual) {
+		return fmt.Errorf("wrong number of constants. expected=%d, actual=%d", len(expected), len(actual))
+	}
+
+	for i, constant := range expected {
+		switch constant := constant.(type) {
+		case int:
+			err := testIntegerObject(int64(constant), actual[i])
+			if err != nil {
+				return fmt.Errorf("constant %d - testIntegerObject failed: %s", i, err)
+			}
+		}
+	}
+
+	return nil
+}
+
+func testIntegerObject(expected int64, actual object.Object) error {
+	result, ok := actual.(*object.Integer)
+	if !ok {
+		return fmt.Errorf("object is not Integer. got=%T (%+v", actual, actual)
+	}
+
+	if result.Value != expected {
+		return fmt.Errorf("object has incorrect value. expected=%d, actual=%d", expected, result.Value)
+	}
+
+	return nil
+}
+
+func testInstructions(
+	expected []code.Instructions,
+	actual code.Instructions,
+) error {
+	concatted := concatInstructions(expected)
+
+	if len(actual) != len(concatted) {
+		return fmt.Errorf("wrong instructions length.\nexpected=%q\nactual=%q", concatted, actual)
+	}
+
+	for i, instruction := range concatted {
+		if actual[i] != instruction {
+			return fmt.Errorf("wrong instruction at %d.\nexpected=%q\nactual=%q", i, concatted, actual)
+		}
+	}
+
+	return nil
+}
+
+func concatInstructions(instructions []code.Instructions) code.Instructions {
+	out := code.Instructions{}
+
+	for _, instruction := range instructions {
+		out = append(out, instruction...)
+	}
+
+	return out
+}
