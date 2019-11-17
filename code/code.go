61404dca2cecde85f1409b8adc075605295ebbc6
Péter Szabó <szaboopeeter@live.com>
2019-11-17 11:05:51 +0700

    Make() bytecode for OpConstant

--- code/code.go
+++ code/code.go
@@ -1,6 +1,9 @@
 package code
 
-import "fmt"
+import (
+	"encoding/binary"
+	"fmt"
+)
 
 type Instructions []byte
 type Opcode byte
@@ -21,8 +24,35 @@ var definitions = map[Opcode]*Definition{
 func Lookup(op byte) (*Definition, error) {
 	definition, ok := definitions[Opcode(op)]
 	if !ok {
-		return nil, fmt.Errorf("Opcode %d is undefined.", op)
+		return nil, fmt.Errorf("opcode %d is undefined", op)
 	}
 
 	return definition, nil
+}
+
+func Make(op Opcode, operands ...int) []byte {
+	definition, ok := definitions[op]
+	if !ok {
+		return []byte{}
+	}
+
+	instructionLength := 1
+	for _, width := range definition.OperandWidths {
+		instructionLength += width
+	}
+
+	instruction := make([]byte, instructionLength)
+	instruction[0] = byte(op)
+
+	offset := 1
+	for i, operand := range operands {
+		width := definition.OperandWidths[i]
+		switch width {
+		case 2:
+			binary.BigEndian.PutUint16(instruction[offset:], uint16(operand))
+		}
+		offset += width
+	}
+
+	return instruction
 }
